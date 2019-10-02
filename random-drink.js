4533f3b0c0fbc687e26de8f8499019b1153e32a4
Emi Rezaei <tomoyose.emi@gmail.com>
2019-10-02 10:46:05 -0700

    current changes

--- random-drink.js
+++ random-drink.js
@@ -8,31 +8,55 @@ function randomDrinks(){
             url: queryURL,
             method: "GET"
         }).then(function(response){
-            console.log(response);
-        let div = $("<div>");
-        let p = $("<p>");
-        let img = $("<img>");
-        let drink = response.drinks;
-        let drinkName = drink[0].strDrink;
-        // div.attr("src", "https://vinepair.com/articles/50-most-popular-cocktails-world-2017/");
-        p.text(drinkName);
-        img.attr("src", drink[0].strDrinkThumb);
-        img.attr("class","drink-option");
-        div.attr("data-name", drinkName);
-        // div.attr("class", "image");
-        div.append(p, img);
-        $("body").append(div);
-
+            // console.log(response);
+            let div = $("<div>");
+            let p = $("<p>");
+            let img = $("<img>");
+            let drink = response.drinks;
+            let drinkName = drink[0].strDrink;
+            // div.attr("src", "https://vinepair.com/articles/50-most-popular-cocktails-world-2017/");
+            p.text(drinkName);
+            img.attr("src", drink[0].strDrinkThumb);
+            // img.attr("class","drink-option");
+            div.attr("data-name", drinkName);
+            div.addClass("drink-option")
+            // div.attr("class", "image");
+            div.append(p, img);
+            $("body").append(div);
         
-        $("div").on("click", function(event){
-        event.preventDefault();
-        let drinkName = $(this).data("name");
-        })
-           
-       
-    })
+            
+    
+            })          
+        
+        }
     }
-}
+
+            
+    $("body").on("click", '.drink-option', function(event){
+        event.preventDefault();
+        alert("click");
+        let drinkName = $(this).attr("data-name");
+        let drinkDetailURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName;
+
+            $.ajax({
+                url: drinkDetailURL,
+                method: "GET"
+            }).then(function(response){
+                console.log(response);
+            })
+            let div = $("<div>");
+            let img = $("<img>");
+            let p = $("<p>");
+            let drink = response.drinks;
+            img.attr("src", drink[0].strDrinkThumb);
+            div.addClass("drink-option");
+            p.text(drink[0].strInstructions);
+            div.append(p, img);
+            $("body").append(div);
+
+
+        })
+
     $("#search").on("click",function(event){  
         event.preventDefault();
         randomDrinks();
