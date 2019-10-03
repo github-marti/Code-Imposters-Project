235bfd6fefa92ba73c957cd3a01aaa2491d96d3e
Emi Rezaei <tomoyose.emi@gmail.com>
2019-10-02 21:26:26 -0700

    completed random drinks

--- random-drink.js
+++ random-drink.js
@@ -1,28 +1,35 @@
+$("#random-submit").on("click",function(){
+    randomDrinks();
+})
 function randomDrinks(){
-    
+    $("#start-container").remove();
     let queryURL =  "https://www.thecocktaildb.com/api/json/v2/9973533/random.php";
     
+    let container = $("<div>");
+    container.attr("id", "random-container");
+    container.addClass("option-container");
+    $("body").append(container);
     
     for(let i = 0; i < 3; i++){
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
+
             // console.log(response);
             let div = $("<div>");
             let p = $("<p>");
             let img = $("<img>");
             let drink = response.drinks;
             let drinkName = drink[0].strDrink;
-            // div.attr("src", "https://vinepair.com/articles/50-most-popular-cocktails-world-2017/");
             p.text(drinkName);
+            p.attr("style", "color: white");
             img.attr("src", drink[0].strDrinkThumb);
-            // img.attr("class","drink-option");
+            img.addClass("option-pic");       
             div.attr("data-name", drinkName);
             div.addClass("drink-option")
-            // div.attr("class", "image");
             div.append(p, img);
-            $("body").append(div);
+            container.append(div);
         
             
     
@@ -34,7 +41,12 @@ function randomDrinks(){
             
     $("body").on("click", '.drink-option', function(event){
         event.preventDefault();
-        alert("click");
+        $("#random-container").remove();
+        let container = $("<div>");
+        container.addClass("option-container");
+         $("body").append(container);
+    
+        
         let drinkName = $(this).attr("data-name");
         let drinkDetailURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName;
 
@@ -43,18 +55,36 @@ function randomDrinks(){
                 method: "GET"
             }).then(function(response){
                 console.log(response);
-            })
+            
             let div = $("<div>");
             let img = $("<img>");
+            let title = $("<h1>").text(drinkName);
             let p = $("<p>");
+            let glassP = $("<p>");
+            let recipeDiv = $("<div>");
+            title.attr("style", "color: white");
+            p.attr("style", "color: white");
+            recipeDiv.attr("style", "color: white");
+            glassP.attr("style", "color: white");
             let drink = response.drinks;
             img.attr("src", drink[0].strDrinkThumb);
+            glassP.text("Glass: " +drink[0].strGlass);
+            img.addClass("option-pic");
             div.addClass("drink-option");
-            p.text(drink[0].strInstructions);
-            div.append(p, img);
-            $("body").append(div);
+            p.text("Recipe: " + drink[0].strInstructions);
+            div.append(title, img, recipeDiv, p, glassP);
+            
 
+            for (let i = 1; i < 16; i++) {
+                if (response.drinks[0][`strIngredient${i}`] !== null) {
+                    console.log('currently on', response.drinks['0'][`strIngredient${i}`])
+                    let ingredientP = $('<p>').text(i+': ' + response.drinks['0'][`strIngredient${i}`]);
+                    recipeDiv.append(ingredientP);
+                }
+            }
+            container.append(div);
 
+        })
         })
 
     $("#search").on("click",function(event){  
